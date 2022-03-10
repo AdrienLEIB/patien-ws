4ed983dd103e1c0c8b5981154f7714e2d9d4e218
Adrien Leib <adrienleib@MacBook-Pro-de-Adrien.local>
2022-03-10 12:33:01 +0100

    first commit

---
+++ src/main/java/com/ynov/medical/patientws/PatientWsApplication.java
@@ -0,0 +1,13 @@
+package com.ynov.medical.patientws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ynov/medical/patientws/PatientWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ynov.medical.patientws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
