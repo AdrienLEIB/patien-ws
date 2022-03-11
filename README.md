b85de383c5dc32a3a159d5a8692febabdff2ffec
Adrien Leib <adrienleib@MacBook-Pro-de-Adrien.local>
2022-03-11 19:41:53 +0100

    add findById and save

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 # patien-ws
+___________________
+
+# pré-requis :
+
+1. mongo d'installé et lancé en local sur le port 27017 (defaut)
+
+- sur MacOS
+brew tap mongodb/brew
+brew install mongodb-community@5.0
+mongo
 
 
-Download spring : https://start.spring.io
+- sur Windows
+
+https://docs.mongodb.com/manual/tutorial/install-mongodb-on-windows/
+
+
+2. Deployez l'application sur un snapshot 
+cd racineduProjet/
+mvn clean install
+
+3. Faites tourner l'application dans un JVM
+mvn clean spring-boot:run
--- src/main/java/com/ynov/medical/patientws/controller/PatientController.java
+++ src/main/java/com/ynov/medical/patientws/controller/PatientController.java
@@ -1,10 +1,17 @@
 package com.ynov.medical.patientws.controller;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ynov.medical.patiensws.model.Patient;
 import com.ynov.medical.patientws.service.PatientService;
@@ -18,6 +25,23 @@ public class PatientController {
 	@GetMapping("/patients")
 	public List<Patient> getAll() {
 		return patientService.getAllPatients();
+	}
+
+	@PostMapping("/patient/add")
+	public ResponseEntity<Patient> createPatient(@RequestBody Patient p) {
+		Patient patientAdded = patientService.createPatient(p);
+		if (Objects.isNull(patientAdded)) {
+			return ResponseEntity.noContent().build();
+		}
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(patientAdded.getId()).toUri();
+		return ResponseEntity.created(location).build();
+	}
+
+	@GetMapping("/patients/{id}")
+	public Patient getPatientById(@PathVariable String id) {
+
+		return patientService.findPatientById(id);
 	}
 
 }
