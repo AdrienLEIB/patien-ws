068ca2a2bee025a7bcb77a9fb72d60ed52104e1a
Adrien Leib <adrienleib@MacBook-Pro-de-Adrien.local>
2022-03-23 15:14:10 +0100

    add swagger

--- src/main/java/com/ynov/medical/patientws/PatientWsApplication.java
+++ src/main/java/com/ynov/medical/patientws/PatientWsApplication.java
@@ -2,8 +2,9 @@ package com.ynov.medical.patientws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class PatientWsApplication {
 
 	public static void main(String[] args) {
